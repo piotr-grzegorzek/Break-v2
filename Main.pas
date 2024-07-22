411891a6c43c000b1d886902cc70e21be2a0e342
piotr-grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-07-22 11:34:26 +0200

    refactor: simplify the main code
    - const var for preferences path
    - F prefix for fields
    - save settings method
    - load settings method
    - remove redundant object accessors
    - restore from tray method
    - Inc built in method
    - Update label method

--- Main.pas
+++ Main.pas
@@ -5,7 +5,6 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls,
-
   IniFiles, Settings;
 
 type
@@ -20,14 +19,19 @@ type
     procedure FormCreate(Sender: TObject);
     procedure FormResize(Sender: TObject);
     procedure btnSettingsClick(Sender: TObject);
-    procedure ToTray;
     procedure btnOKClick(Sender: TObject);
     procedure TrayIcon1Click(Sender: TObject);
     procedure WorkTimerTimer(Sender: TObject);
   private
-    workTime: Integer;
-    breakTime: Integer;
-    currentTime: Integer;
+    FWorkTime: Integer;
+    FBreakTime: Integer;
+    FCurrentTime: Integer;
+    procedure LoadSettings;
+    procedure SaveSettings(AWorkTime, ABreakTime: Integer);
+    procedure UpdateLabel;
+    procedure ToTray;
+    procedure RestoreFromTray;
+    procedure AppToFront;
   public
     { Public declarations }
   end;
@@ -39,12 +43,15 @@ implementation
 
 {$R *.dfm}
 
-procedure AppToFront;
+const
+  SETTINGS_FILE = './preferences.ini';
+
+procedure TForm1.AppToFront;
 var
   Input: TInput;
 begin
   ZeroMemory(@Input, SizeOf(Input));
-  SendInput(1, Input, SizeOf(Input)); // don't send anyting actually to another app..
+  SendInput(1, Input, SizeOf(Input)); // don't send anything actually to another app..
   SetForegroundWindow(Application.Handle);
 end;
 
@@ -52,7 +59,7 @@ procedure TForm1.btnOKClick(Sender: TObject);
 begin
   if Label1.Caption = '0' then
   begin
-    if workTime > 0 then
+    if FWorkTime > 0 then
     begin
       WorkTimer.Enabled := True;
     end;
@@ -64,42 +71,23 @@ procedure TForm1.btnSettingsClick(Sender: TObject);
 var
   AWorkTime: Integer;
   ABreakTime: Integer;
-  IniFile: TIniFile;
 begin
-  IniFile := TIniFile.Create('./preferences.ini');
-  try
-    AWorkTime := IniFile.ReadInteger('Settings', 'WorkTime', 0);
-    ABreakTime := IniFile.ReadInteger('Settings', 'BreakTime', 0);
-    if modShowSettings(AWorkTime, ABreakTime) then
-    begin
-      IniFile.WriteInteger('Settings', 'WorkTime', AWorkTime);
-      IniFile.WriteInteger('Settings', 'BreakTime', ABreakTime);
-      workTime := AWorkTime;
-      breakTime := ABreakTime;
-    end;
-  finally
-    IniFile.Free;
+  AWorkTime := FWorkTime;
+  ABreakTime := FBreakTime;
+  if modShowSettings(AWorkTime, ABreakTime) then
+  begin
+    SaveSettings(AWorkTime, ABreakTime);
+    FWorkTime := AWorkTime;
+    FBreakTime := ABreakTime;
   end;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
-var
-  AWorkTime: Integer;
-  ABreakTime: Integer;
-  IniFile: TIniFile;
 begin
-  Form1.Color := TColor(RGB(24, 24, 24));
+  Color := TColor(RGB(24, 24, 24));
   ToTray;
-
-  IniFile := TIniFile.Create('./preferences.ini');
-  try
-    workTime := IniFile.ReadInteger('Settings', 'WorkTime', 0);
-    breakTime := IniFile.ReadInteger('Settings', 'BreakTime', 0);
-  finally
-    IniFile.Free;
-  end;
-
-  if workTime > 0 then
+  LoadSettings;
+  if FWorkTime > 0 then
   begin
     WorkTimer.Enabled := True;
   end;
@@ -107,15 +95,15 @@ end;
 
 procedure TForm1.FormResize(Sender: TObject);
 begin
-  btnOK.Left := Form1.Width - btnOK.Width - 8;
-  btnBreak.Left := Form1.Width - btnBreak.Width - 8;
-  btnBreak.Top := Form1.Height - btnBreak.Height - 8;
+  btnOK.Left := Width - btnOK.Width - 8;
+  btnBreak.Left := Width - btnBreak.Width - 8;
+  btnBreak.Top := Height - btnBreak.Height - 8;
   btnSettings.Top := btnBreak.Top;
 end;
 
 procedure TForm1.ToTray;
 begin
-  Self.Hide;
+  Hide;
   WindowState := wsMinimized;
   TrayIcon1.Visible := True;
   Application.ShowMainForm := False;
@@ -124,8 +112,13 @@ end;
 
 procedure TForm1.TrayIcon1Click(Sender: TObject);
 begin
-  Self.Show;
-  WindowState := wsMaximized;
+  RestoreFromTray;
+end;
+
+procedure TForm1.RestoreFromTray;
+begin
+  Show;
+  WindowState := wsNormal;
   TrayIcon1.Visible := False;
   ShowWindow(Application.Handle, SW_SHOWNORMAL);
   AppToFront;
@@ -133,25 +126,59 @@ end;
 
 procedure TForm1.WorkTimerTimer(Sender: TObject);
 begin
-  if currentTime + 1 < workTime then
+  if FCurrentTime + 1 < FWorkTime then
   begin
     // work continues
-    currentTime := currentTime + 1;
-    Label1.Caption := IntToStr(workTime - currentTime);
+    Inc(FCurrentTime);
+    UpdateLabel;
   end
-  else if currentTime + 1 = workTime then
+  else if FCurrentTime + 1 = FWorkTime then
   begin
     // work ended
-
-    TrayIcon1Click(nil);
-    currentTime := currentTime + 1;
-    Label1.Caption := IntToStr(workTime - currentTime);
+    RestoreFromTray;
+    Inc(FCurrentTime);
+    UpdateLabel;
   end
   else
   begin
     WorkTimer.Enabled := False;
-    currentTime := 0;
+    FCurrentTime := 0;
   end;
+end;
+
+procedure TForm1.LoadSettings;
+var
+  IniFile: TIniFile;
+begin
+  IniFile := TIniFile.Create(SETTINGS_FILE);
+  try
+    FWorkTime := IniFile.ReadInteger('Settings', 'WorkTime', 0);
+    FBreakTime := IniFile.ReadInteger('Settings', 'BreakTime', 0);
+  except
+    on E: Exception do
+      ShowMessage('Error reading settings: ' + E.Message);
+  end;
+  IniFile.Free;
+end;
+
+procedure TForm1.SaveSettings(AWorkTime, ABreakTime: Integer);
+var
+  IniFile: TIniFile;
+begin
+  IniFile := TIniFile.Create(SETTINGS_FILE);
+  try
+    IniFile.WriteInteger('Settings', 'WorkTime', AWorkTime);
+    IniFile.WriteInteger('Settings', 'BreakTime', ABreakTime);
+  except
+    on E: Exception do
+      ShowMessage('Error saving settings: ' + E.Message);
+  end;
+  IniFile.Free;
+end;
+
+procedure TForm1.UpdateLabel;
+begin
+  Label1.Caption := IntToStr(FWorkTime - FCurrentTime);
 end;
 
 end.
