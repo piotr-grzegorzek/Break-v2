f88a087169441cffda735ea7842c232ab55870db
piotr-grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-07-22 12:02:35 +0200

    refactor: reorder unit members, add comments,

--- Main.pas
+++ Main.pas
@@ -17,23 +17,30 @@ type
     TrayIcon1: TTrayIcon;
     WorkTimer: TTimer;
     BreakTimer: TTimer;
+
+    // Event Handlers
     procedure FormCreate(Sender: TObject);
     procedure FormResize(Sender: TObject);
     procedure btnSettingsClick(Sender: TObject);
     procedure btnOKClick(Sender: TObject);
+    procedure btnBreakClick(Sender: TObject);
     procedure TrayIcon1Click(Sender: TObject);
     procedure WorkTimerTimer(Sender: TObject);
-    procedure btnBreakClick(Sender: TObject);
     procedure BreakTimerTimer(Sender: TObject);
+
   private
+    // Private Fields
     FWorkTime: Integer;
     FBreakTime: Integer;
     FCurrentTime: Integer;
+
+    // Private Methods
     procedure LoadSettings;
     procedure SaveSettings(AWorkTime, ABreakTime: Integer);
     procedure ToTray;
     procedure RestoreFromTray;
     procedure AppToFront;
+
   public
     { Public declarations }
   end;
@@ -48,60 +55,7 @@ implementation
 const
   SETTINGS_FILE = './preferences.ini';
 
-procedure TForm1.AppToFront;
-var
-  Input: TInput;
-begin
-  ZeroMemory(@Input, SizeOf(Input));
-  SendInput(1, Input, SizeOf(Input));
-  SetForegroundWindow(Application.Handle);
-end;
-
-procedure TForm1.BreakTimerTimer(Sender: TObject);
-begin
-  Inc(FCurrentTime);
-  Label1.Caption := IntToStr(FBreakTime - FCurrentTime);
-  if FCurrentTime >= FBreakTime then
-  begin
-    // break ended
-    BreakTimer.Enabled := False;
-    FCurrentTime := 0;
-  end;
-end;
-
-procedure TForm1.btnBreakClick(Sender: TObject);
-begin
-  if (Label1.Caption = '0') and (FBreakTime > 0) then
-  begin
-    Label1.Caption := IntToStr(FBreakTime - FCurrentTime);
-    BreakTimer.Enabled := True;
-  end;
-end;
-
-procedure TForm1.btnOKClick(Sender: TObject);
-begin
-  if (Label1.Caption = '0') and (FWorkTime > 0) then
-  begin
-    Label1.Caption := IntToStr(FWorkTime - FCurrentTime);
-    WorkTimer.Enabled := True;
-  end;
-  ToTray;
-end;
-
-procedure TForm1.btnSettingsClick(Sender: TObject);
-var
-  AWorkTime: Integer;
-  ABreakTime: Integer;
-begin
-  AWorkTime := FWorkTime;
-  ABreakTime := FBreakTime;
-  if modShowSettings(AWorkTime, ABreakTime) then
-  begin
-    SaveSettings(AWorkTime, ABreakTime);
-    FWorkTime := AWorkTime;
-    FBreakTime := ABreakTime;
-  end;
-end;
+  // Event Handlers
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
@@ -122,27 +76,43 @@ begin
   btnSettings.Top := btnBreak.Top;
 end;
 
-procedure TForm1.ToTray;
+procedure TForm1.btnSettingsClick(Sender: TObject);
+var
+  AWorkTime: Integer;
+  ABreakTime: Integer;
 begin
-  Hide;
-  WindowState := wsMinimized;
-  TrayIcon1.Visible := True;
-  Application.ShowMainForm := False;
-  ShowWindow(Application.Handle, SW_HIDE);
+  AWorkTime := FWorkTime;
+  ABreakTime := FBreakTime;
+  if modShowSettings(AWorkTime, ABreakTime) then
+  begin
+    SaveSettings(AWorkTime, ABreakTime);
+    FWorkTime := AWorkTime;
+    FBreakTime := ABreakTime;
+  end;
+end;
+
+procedure TForm1.btnOKClick(Sender: TObject);
+begin
+  if (Label1.Caption = '0') and (FWorkTime > 0) then
+  begin
+    Label1.Caption := IntToStr(FWorkTime - FCurrentTime);
+    WorkTimer.Enabled := True;
+  end;
+  ToTray;
+end;
+
+procedure TForm1.btnBreakClick(Sender: TObject);
+begin
+  if (Label1.Caption = '0') and (FBreakTime > 0) then
+  begin
+    Label1.Caption := IntToStr(FBreakTime - FCurrentTime);
+    BreakTimer.Enabled := True;
+  end;
 end;
 
 procedure TForm1.TrayIcon1Click(Sender: TObject);
 begin
   RestoreFromTray;
-end;
-
-procedure TForm1.RestoreFromTray;
-begin
-  Show;
-  WindowState := wsMaximized;
-  TrayIcon1.Visible := False;
-  ShowWindow(Application.Handle, SW_SHOWNORMAL);
-  AppToFront;
 end;
 
 procedure TForm1.WorkTimerTimer(Sender: TObject);
@@ -157,6 +127,20 @@ begin
     FCurrentTime := 0;
   end;
 end;
+
+procedure TForm1.BreakTimerTimer(Sender: TObject);
+begin
+  Inc(FCurrentTime);
+  Label1.Caption := IntToStr(FBreakTime - FCurrentTime);
+  if FCurrentTime >= FBreakTime then
+  begin
+    // break ended
+    BreakTimer.Enabled := False;
+    FCurrentTime := 0;
+  end;
+end;
+
+// Private Methods
 
 procedure TForm1.LoadSettings;
 var
@@ -186,6 +170,33 @@ begin
       ShowMessage('Error saving settings: ' + E.Message);
   end;
   IniFile.Free;
+end;
+
+procedure TForm1.ToTray;
+begin
+  Hide;
+  WindowState := wsMinimized;
+  TrayIcon1.Visible := True;
+  Application.ShowMainForm := False;
+  ShowWindow(Application.Handle, SW_HIDE);
+end;
+
+procedure TForm1.RestoreFromTray;
+begin
+  Show;
+  WindowState := wsMaximized;
+  TrayIcon1.Visible := False;
+  ShowWindow(Application.Handle, SW_SHOWNORMAL);
+  AppToFront;
+end;
+
+procedure TForm1.AppToFront;
+var
+  Input: TInput;
+begin
+  ZeroMemory(@Input, SizeOf(Input));
+  SendInput(1, Input, SizeOf(Input));
+  SetForegroundWindow(Application.Handle);
 end;
 
 end.
